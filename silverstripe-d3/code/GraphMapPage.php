e1e69f902eff820279b6a2ea29bfb45a0b5b56fa
Josh Kosmala <josh@novaweb.co.nz>
2015-11-07 16:19:31 +1300

    d3 functioning

--- silverstripe-d3/code/GraphMapPage.php
+++ silverstripe-d3/code/GraphMapPage.php
@@ -9,6 +9,8 @@ class GraphMapPage_Controller extends LocationMapPage_Controller {
    public function init(){
       parent::init();
       Requirements::javascript(MODULE_D3_DIR . "/javascript/d3.v3.min.js");
+      Requirements::javascript(MODULE_D3_DIR . "/javascript/d3-config.js");
+      Requirements::javascript(MODULE_D3_DIR . "/javascript/flare.json");
    }
 
 }
