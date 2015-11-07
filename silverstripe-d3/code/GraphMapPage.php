68587095e44f907abceb348eaad7e1002135790f
Josh Kosmala <josh@novaweb.co.nz>
2015-11-07 15:01:02 +1300

    Roll back to core 3.1.13

--- silverstripe-d3/code/GraphMapPage.php
+++ silverstripe-d3/code/GraphMapPage.php
@@ -6,4 +6,9 @@ class GraphMapPage extends LocationMapPage {
 
 class GraphMapPage_Controller extends LocationMapPage_Controller {
 
+   public function init(){
+      parent::init();
+      Requirements::javascript(MODULE_D3_DIR . "/javascript/d3.v3.min.js");
+   }
+
 }
